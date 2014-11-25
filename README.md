40dd895c5a3d9b424f3065dd6cdd9a2fb95f8af0
Cesar Kastli <cesarkastli@gmail.com>
2014-11-25 13:22:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ensure-authenticated
+===
+
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var extend = require('util')._extend
+
+module.exports = function(options) {
+  var defaults = {
+    redirectUrl: '/',
+    message: 'Login required',
+    setReturnTo: true
+  }
+
+  if ('string' == typeof options) {
+    options = { redirectUrl: options }
+  }
+
+  options = extend(defaults, options || {})
+
+  return function *ensureAuthenticatedMiddleware(next) {
+    if (!this.isAuthenticated()) {
+      if (this.flash && options.message) {
+        this.flash('error', options.message)
+      }
+      if (this.session && options.setReturnTo) {
+        this.session.returnTo = this.originalUrl || this.req.url
+      }
+      return this.redirect(options.redirectUrl || '/')
+    }
+    yield next
+  }
+}
